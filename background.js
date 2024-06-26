3facb9e17f5a04b1c8699250ce32bbf12024f05b
pinkartemis <47722730+pinkartemis@users.noreply.github.com>
2024-06-26 13:26:37 -0500

    created checkPrices function

--- background.js
+++ background.js
@@ -8,3 +8,20 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     }
 });
 
+function checkPrices() {
+    chrome.storage.local.get(['trackedUrls'], (result) => {
+        const urls = result.trackedUrls || [];
+        urls.forEach(url => {
+            chrome.tabs.create({url: url, activate: false }, (tab) => {
+                chrome.tabs.executeScript(tab.id, { file: 'content.js'}, () => {
+                    chrome.tabs.sendMessage(tab.id, {action: "scrapePrices"}, (response) => {
+                        if (response && response.prices){
+                            compareAndNotify(url, response.prices);
+                        }
+                        chrome.tabs.remove(tab.id);
+                    });
+                });
+            });
+        });
+    });
+}
